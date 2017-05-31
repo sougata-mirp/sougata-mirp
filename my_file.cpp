9f2d32e318433dffb0cc34ea7a82da1f0a7c36f5
somajhi-mirp <somajhi-mirp@mit.edu>
2017-05-31 06:49:26 +0530

    Adding file testjit

---
+++ my_file.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+float a,b,c,root1,root2,delta
+cout <<"Enter three numbers a,b,c of"<<"ax2 + bx +c";
+cin >> a >> b >> C
+if(!a)  //if a is zero
+cout<<"The value of a should not be zero" <<"\n Abosting!!!!!"<<"\n"
+else
+{
+delta=b*b-4*a*c;  //begining of else's body
+if(delta>0)
+{
+root1=(-b+ sqrt
